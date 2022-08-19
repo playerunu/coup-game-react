82ecfef6c316ed7f0a964567e86359095727dc53
Mihaita Leoveanu <playerunu@gmail.com>
2022-08-19 22:27:11 +0200

    Added image utils

---
+++ src/utils/image/index.ts
@@ -0,0 +1,3 @@
+export * from './images';
+export * from './detectWebp';
+export * from './provider';
---
+++ src/utils/image/provider.tsx
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { detectWebP } from './detectWebp';
+import { getImage } from './images';
+
+const ImageContext = React.createContext({
+  webpSupported: false,
+});
+
+export const ImageProvider: React.FC = ({ children }) => {
+  const [isSupported, setIsSupported] = useState<boolean>(false);
+
+  useEffect(() => {
+    const detect = async () => {
+      const supported = await detectWebP('lossless');
+      setIsSupported(supported);
+    };
+
+    detect();
+  }, []);
+
+  return (
+    <ImageContext.Provider
+      value={{
+        webpSupported: isSupported,
+      }}
+    >
+      {children}
+    </ImageContext.Provider>
+  );
+};
+
+export const useWebpImage = (...urls: string[]): string[] => {
+  const { webpSupported } = useContext(ImageContext);
+
+  const images = urls.map((url) => {
+    return getImage(url, webpSupported);
+  });
+
+  return images;
+};

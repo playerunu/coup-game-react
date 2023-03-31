32e25776d29f298b176cdb1f634a59ba35eda598
Mihaita Leoveanu <playerunu@gmail.com>
2023-03-31 19:27:40 +0200

    Added svg logos for each influence

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,9 +1,13 @@
 export const BACKGROUND_COLOR = 'rgba(40, 131, 185, 0.4)';
-
 export const SHADOW_COLOR = 'rgba(36, 14, 24, 0.49)';
 
 export const CURSOR_BACKGROUND = 'rgba(195, 109, 217, 0.09)';
-
 export const CURSOR_BORDER = 'rgba(149, 51, 174, 0.23)';
+
+export const AMBASADOR_LOGO_COLOR = 'rgb(165, 203, 14)';
+export const ASSASSIN_LOGO_COLOR = '#2D2727';
+export const CAPTAIN_LOGO_COLOR = ' #73A7E0';
+export const CONTESSA_LOGO_COLOR = '#E03730';
+export const DUKE_LOGO_COLOR = '#E83873';
 
 export const SMALL_SCREEN_THEME_BREAKPOINT = 'sm';
